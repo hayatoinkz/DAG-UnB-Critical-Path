2c3d96aad055fc1ca2282e35a2df4c69cb7812cb
Vinicius Rocha <vinicius.r.s@globo.com>
2019-10-10 10:39:52 -0300

    Testes na Main

--- main.cpp
+++ main.cpp
@@ -9,8 +9,17 @@ int main()
   vector<Materia *> Vertices = Read(file + ".gml");
   MakeDOT(Vertices);
   system("dot -Tpng grafo.dot -o grafo.png");
+  for (int i = 0; i < Vertices.size(); i++)
+  {
+    cout << i + 1 << " " + Vertices[i]->nome + " " << Vertices[i]->id << endl;
+  }
+  cout << "-----------------------------------------------------------------------------------" << endl;
   vector<Materia *> Grafo_Ordenado = OrganizacaoTopologica(Vertices);
   MakeDOT(Grafo_Ordenado);
+  for (int i = 0; i < Grafo_Ordenado.size(); i++)
+  {
+    cout << i + 1 << " " + Grafo_Ordenado[i]->nome + " " << Grafo_Ordenado[i]->id << endl;
+  }
   system("dot -Tpng grafo.dot -o grafo_Ordenado.png");
   return 0;
 }
